4bb457d909bd3b9aa94e54a9e3e79d04a04822c3
guy-car <guil.crn@gmail.com>
2025-06-09 18:52:12 -0400

    completed part 2

---
+++ tools/chat-store.ts
@@ -0,0 +1,35 @@
+import { generateId } from 'ai';
+import { type Message } from 'ai';
+import { existsSync, mkdirSync } from 'fs';
+import { writeFile, readFile } from 'fs/promises';
+import path from 'path';
+
+export async function createChat(): Promise<string> {
+  const id = generateId(); // generate a unique chat ID
+  await writeFile(getChatFile(id), '[]'); // create an empty chat file
+  return id;
+}
+
+export async function loadChat(id: string): Promise<Message[]> {
+  console.log('hey I am loading')
+  const file = await readFile(getChatFile(id), 'utf8')
+  const messages = JSON.parse(file) as Message[];
+  return messages
+}
+
+function getChatFile(id: string): string {
+  const chatDir = path.join(process.cwd(), '.chats');
+  if (!existsSync(chatDir)) mkdirSync(chatDir, { recursive: true });
+  return path.join(chatDir, `${id}.json`);
+}
+
+export async function saveChat({
+  id,
+  messages,
+}: {
+  id: string;
+  messages: Message[];
+}): Promise<void> {
+  const content = JSON.stringify(messages, null, 2);
+  await writeFile(getChatFile(id), content);
+}
