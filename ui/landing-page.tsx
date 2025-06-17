604bf402a3ed1431ddd0d1a953b48ce11f8b211d
guy-car <guil.crn@gmail.com>
2025-06-16 20:28:53 -0400

    remove duplicate sidebar component

--- ui/landing-page.tsx
+++ ui/landing-page.tsx
@@ -1,14 +1,11 @@
 'use server'
 
-import AppSidebar from "~/app/_components/server/AppSidebar"; // Direct import
 import { NewChatComponent } from './NewChatComponent';
 
 export async function LandingPage() {
 
     return (
-        <div className='flex'>
-            {/* <Header /> */}
-            <AppSidebar /> {/* Added here temporarily */}
+        <div>
             <NewChatComponent />
         </div>
     )
